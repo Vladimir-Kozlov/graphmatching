fd908f8df468e12284580a311ad46ce37ff8be09
vladimir_kozlov <kozlov2.volodia2@gmail.com>
2019-08-06 22:35:29 +0300

    rewrite feature map index layer

--- deep/layers/utils.py
+++ deep/layers/utils.py
@@ -3,37 +3,56 @@ import tensorflow as tf
 keras = tf.keras
 
 
-def idxtransform(idx, transform=lambda x: x // 32):
-    # Transform coordinates of image points to index feature vectors in high layers
+# def idxtransform(idx, transform=lambda x: x // 32):
+#     # Transform coordinates of image points to index feature vectors in high layers
+#     # Input: idx: point coordinates of shape [batch size, number of points, 2]
+#     #        transform: index transformation function; by default, we simply divide them by 32 (corresponding to feature map size in CNN)
+#     # Output: transformed indices with added leading batch number
+#     x = transform(idx)
+#     r = tf.reshape(tf.range(tf.shape(idx)[0]), [-1, 1, 1]) #create range of batch numbers
+#     r = tf.tile(r, tf.concat([[1], tf.shape(idx)[1:-1], [1]], axis=0))
+#     return tf.concat([r, x], axis=-1)
+
+
+# class FMapIndexLayer(keras.layers.Layer):
+#     # Performs feature indexing from feature map
+#     def __init__(self, transform=lambda x: x // 32, **kwargs):
+#         self.transform = transform
+#         super(FMapIndexLayer, self).__init__(**kwargs)
+
+#     def build(self, input_shape):
+#         assert isinstance(input_shape, (list, tuple))
+#         super(FMapIndexLayer, self).build(input_shape)
+
+#     def call(self, x):
+#         # Input: img: images, [batch size, height, width, channel]
+#         #        idx: keypoint coordinates, [batch size, number of points, 2]
+#         # Output: keypoint features, [batch size, number of points, channel]
+#         idx = idxtransform(x[1], transform=self.transform)
+#         return tf.gather_nd(x[0], idx)
+
+#     def compute_output_shape(self, input_shape):
+#         assert isinstance(input_shape, (list, tuple))
+#         return (input_shape[1][0], input_shape[0][-1])
+
+
+def idxtransform(idx):
+    # Add leading batch number to indices
     # Input: idx: point coordinates of shape [batch size, number of points, 2]
-    #        transform: index transformation function; by default, we simply divide them by 32 (corresponding to feature map size in CNN)
-    # Output: transformed indices with added leading batch number
-    x = transform(idx)
+    # Output: indices with added leading batch number
     r = tf.reshape(tf.range(tf.shape(idx)[0]), [-1, 1, 1]) #create range of batch numbers
     r = tf.tile(r, tf.concat([[1], tf.shape(idx)[1:-1], [1]], axis=0))
-    return tf.concat([r, x], axis=-1)
+    return tf.concat([r, idx], axis=-1)
 
 
-class FMapIndexLayer(keras.layers.Layer):
+class FMapIndexLayer(keras.layers.Lambda):
     # Performs feature indexing from feature map
-    def __init__(self, transform=lambda x: x // 32, **kwargs):
-        self.transform = transform
-        super(FMapIndexLayer, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        assert isinstance(input_shape, (list, tuple))
-        super(FMapIndexLayer, self).build(input_shape)
-
-    def call(self, x):
-        # Input: img: images, [batch size, height, width, channel]
-        #        idx: keypoint coordinates, [batch size, number of points, 2]
-        # Output: keypoint features, [batch size, number of points, channel]
-        idx = idxtransform(x[1], transform=self.transform)
-        return tf.gather_nd(x[0], idx)
-
-    def compute_output_shape(self, input_shape):
-        assert isinstance(input_shape, (list, tuple))
-        return (input_shape[1][0], input_shape[0][-1])
+    # Input: img: images, [batch size, height, width, channel]
+    #        idx: keypoint coordinates, [batch size, number of points, 2]
+    # Output: keypoint features, [batch size, number of points, channel]
+    def __init__(self, **kwargs):
+        f = lambda x: tf.gather_nd(x[0], idxtransform(x[1]))
+        super(FMapIndexLayer, self).__init__(function=f, **kwargs)
 
 
 class EdgeAttributeLayer(keras.layers.Lambda):
@@ -46,4 +65,3 @@ class EdgeAttributeLayer(keras.layers.Lambda):
 		if isinstance(attr_func, str):
 			attr_func = self.__alias[attr_func]
 		super(EdgeAttributeLayer, self).__init__(function=attr_func, **kwargs)
-
