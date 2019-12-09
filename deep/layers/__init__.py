da97f65cc2e6d280b81f853364d4f3e60c98c273
vladimir_kozlov <kozlov2.volodia2@gmail.com>
2019-12-09 05:52:40 +0300

    fix imports

--- deep/layers/__init__.py
+++ deep/layers/__init__.py
@@ -1,3 +1,4 @@
 from .utils import FMapIndexLayer, EdgeFeatExtract, EdgeAttributeLayer, DummyFeaturesLayer, DummyEdgeLayer
 from .affinity import AffinityLayer
 from .simproc import PowerIterationLayer, SinkhornIterationLayer
+from .graph import AdjacencyMatrixLayer, GraphConvEdgeLayer, GraphConvVertexLayer
